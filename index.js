8d6bfb70ec6bc8b793eb1a5b62701d9c3cded699
Subhashis Das <109717131+subh93@users.noreply.github.com>
2023-03-14 10:54:00 +0530

    Back to the previous 'Access-Control-Allow-Origin'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
 
 //     Website you wish to allow to connect
 //     res.setHeader('Access-Control-Allow-Origin', 'https://zany-ruby-octopus-boot.cyclic.app');
-       res.setHeader('Access-Control-Allow-Origin', true);
+       res.setHeader('Access-Control-Allow-Origin', '*');
     
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
