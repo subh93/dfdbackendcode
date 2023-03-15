ff3c2a6cc41ceb97daca588c32b60cd9843d2add
Subhashis Das <109717131+subh93@users.noreply.github.com>
2023-03-15 08:41:16 +0530

    Change the entire code of index.js

--- index.js
+++ index.js
@@ -1,58 +1,40 @@
-const db = require('./db');
-
 const express = require('express');
 const app = express();
-
+const bodyParser  = require('body-parser')
 const dotenv = require('dotenv');
-// const cors = require('cors');
-const bodyParser = require('body-parser');
-const { Console } = require('console');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 dotenv.config();
-// app.use(cors({origin: '*'}));
 
-app.use(function (req, res, next) {
+const FoodItemsRoute = require('./routes/foodItemsRoutes')
+const FoodCategoriesRoute = require('./routes/foodCategoryRoutes')
+const UserRoute = require('./routes/userRoute')
+const OrdersRoute = require('./routes/ordersRoute')
 
-//     Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'https://zany-ruby-octopus-boot.cyclic.app');
-       res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.send("Server Working");
 })
 
-const FoodItemsRoute = require('./routes/foodItemsRoutes')
-const FoodCategoriesRoute = require('./routes/foodCategoryRoutes')
-const UserRoute = require('./routes/userRoute')
-const ordersRoute = require('./routes/ordersRoute')
-
 app.use('/api', FoodItemsRoute);
 app.use('/api', FoodCategoriesRoute);
 app.use('/api', UserRoute);
-app.use('/api', ordersRoute);
+app.use('/api', OrdersRoute);
 
+mongoose.set('strictQuery', true);
+const connectiondone = async() => {
+  try {
+    const dbconnect = await mongoose.connect(process.env.MONGOURL,{useNewUrlParser: true})
+    console.log(`Connection Establish ${dbconnect.connection.host}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-// app.listen(process.env.PORT, async()=>{
-//     await db;
-//     console.log(`Server is running on ${process.env.PORT}`)
-// })
-
-db().then(()=>{
+connectiondone().then(()=>{
     app.listen(process.env.PORT,()=>{
     console.log(`Listening on port ${process.env.PORT}`)
   })
